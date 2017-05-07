95b8b4aadffae459397e008e649694b96fcbc12e
Scott Wilkins <scottwilkins@Scotts-mac.local>
2017-05-07 11:11:20 -0600

    add image loaders

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
-const sum = require('./sum')
+import sum from './sum'
+import './image_viewer'
 console.log(sum(2,3))
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,47 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 const config = {
   entry: './src/index.js',
   output: {
     path:  path.resolve(__dirname, 'build'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    publicPath: 'build/'
   },
   module: {
     rules: [
       {
         use: 'babel-loader',
         test: /\.js$/
+      },
+      {
+        //this creates style in the header of the html
+        //install 'extract-text-webpack-plugin' to create separate file for faster load time
+        //use: ['style-loader', 'css-loader'],
+        //must use older 'loader' syntax instead of 'use'
+        loader: ExtractTextPlugin.extract({
+          loader: 'css-loader'
+        }),
+        test: /\.css$/
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        use: [
+          {
+            loader: 'image-webpack-loader?',
+            options: {bypassOnDebug: true}
+          },
+          {
+            loader: 'url-loader',
+            options: {limit: 4000}
+          }
+        ]
       }
     ]
-  }
+  },
+  plugins: [
+    new ExtractTextPlugin('style.css')
+  ]
 };
 
 module.exports = config;
